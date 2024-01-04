83f866b0e7ee5c02c6543fc1f92be704123883f3
robertrb63 <81638977+robertrb63@users.noreply.github.com>
2024-01-04 18:54:27 +0100

    Add files via upload

---
+++ microservices-eureka/src/test/java/com/micreservices/eureka/MicroservicesEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micreservices.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
